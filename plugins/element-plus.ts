bc01016ae3c234ab46b2f710131c27a811f2ff27
leida <zhang20084@126.com>
2022-10-09 00:34:41 +0800

    fix:修复select无法刷新的问题

--- plugins/element-plus.ts
+++ plugins/element-plus.ts
@@ -1,6 +1,28 @@
 import { defineNuxtPlugin } from '#app'
 
-import ElementPlus from 'element-plus/dist/index.full.min.js'
+import ElementPlus from 'element-plus'
+import { ID_INJECTION_KEY } from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
 export default defineNuxtPlugin(nuxtApp => {
-  nuxtApp.vueApp.use(ElementPlus)
+  if (process.client) {
+    const app = nuxtApp.vueApp
+    app
+      .provide(ID_INJECTION_KEY, {
+        prefix: Math.floor(Math.random() * 10000),
+        current: 0
+      })
+      .use(ElementPlus, {
+        size: 'small',
+        locale: zhCn
+      })
+
+    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+      if (key === 'Link') {
+        continue
+      }
+      app.component(key, component)
+    }
+  }
 })
