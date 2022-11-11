5842550f01410c6b9ef4d6f167a3f1da06f40df2
leida <zhang20084@126.com>
2022-11-11 15:14:50 +0800

    build:第三方包升级

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,30 +2,11 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  meta: {
-    title: '接口测试工具 - EasyAPI开票机器人',
-    meta: [
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'keywords', name: 'keywords', content: '接口测试工具, EasyAPI开票机器人' }
-    ]
-  },
-  router: {
-    base: '/'
-  },
-  components: true,
   buildModules: ['@pinia/nuxt'],
+  modules: ['@nuxtjs/tailwindcss'],
 
-  css: ['~/assets/css/tailwind.css', 'element-plus/dist/index.css', '~/assets/css/page.css'],
-  build: {
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {}
-        }
-      }
-    }
-  },
+  css: ['element-plus/dist/index.css', '~/assets/css/page.css'],
+  build: {},
   vite: {
     build: {
       chunkSizeWarningLimit: 4096
