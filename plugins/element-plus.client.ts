393fe587f9f922b0ade21d11a172ffd5dfff6a0a
leida <zhang20084@126.com>
2023-10-22 23:19:24 +0800

    fix:第三方包升级

--- plugins/element-plus.client.ts
+++ plugins/element-plus.client.ts
@@ -1,6 +1,7 @@
 import ElementPlus, { ID_INJECTION_KEY } from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import { defineNuxtPlugin } from 'nuxt/app'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(ElementPlus, {
